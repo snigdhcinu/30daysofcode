eb7df33ff2cb1c80a83866b7ff6c5e5f2d08e92e
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-06-13 00:00:30 +0530

    phase-1 done

--- Desktop/Personal Projects/15daysofcode/colorFlicker/script.js
+++ Desktop/Personal Projects/15daysofcode/colorFlicker/script.js
@@ -1,10 +1,16 @@
-
- function change(){
- 	 let array=["red","green","blue","yellow","pink","orange","cyan","voilet","purple","grey","indigo","burgundy","white"]
-	 let textNode=document.getElementById('infoPanel');
+ 	let array=["red","green","blue","yellow","pink","orange","cyan","voilet","purple","grey","indigo","burgundy","white"]
+	let textNode=document.getElementById('infoPanel');
  	let btn=document.getElementById('button');	
  	let container=document.getElementById('container')
+ function change(){
+
+ 	// Erasing existing value
  	let i = Math.floor(Math.random()*array.length);
- 	container.backgroundColor=array[i];
- 	console.log(container.backgroundColor);
+ 	let node=document.createTextNode(" ");
+ 	document.getElementsByTagName('h1')[0].appendChild(node);
+ 	
+ 	container.style.backgroundColor=array[i];
+	node=document.createTextNode(" "+array[i]);
+ 	document.getElementsByTagName('h1')[0].appendChild(node);
+ 	// console.log(container.backgroundColor);
  }
